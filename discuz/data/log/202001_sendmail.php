0cb2448ae7bec3032c01742286c0219e555e4e20
ggjjl1 <ggjjl1@126.com>
2020-04-01 16:06:13 +0800

    删除缓存文件

--- discuz/data/log/202001_sendmail.php
+++
@@ -1,2 +0,0 @@
-<?PHP exit;?>	2020-01-16 00:21:49	5.188.84.45	0	/member.php?mod=register	juddmc86@mail.ru sendmail failed.
-
--- discuz/data/sysdata/cache_mobile.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 867db5b7b030fbd92a701887907b1caf
-
-$mobilecheck = '{"discuzversion":"X3.4","charset":"utf-8","version":"4","pluginversion":"1.4.8","oemversion":"0","regname":"signup","qqconnect":"0","sitename":"24plus.pw\\u2014\\u2014\\u8d30\\u8086\\u793e\\u533a\\uff0c\\u5206\\u4eab\\u751f\\u6d3b\\uff01","mysiteid":"","ucenterurl":"http:\\/\\/localhost:8001\\/uc_server","setting":{"closeforumorderby":null},"extends":{"used":null,"lastupdate":null}}';
-?>
