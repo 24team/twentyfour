eeae7b08066650b661fa11243eac080d542cae69
ggjjl1 <ggjjl1@126.com>
2020-11-12 23:00:35 +0800

    删除小组件

--- discuz/data/log/201912_illegallog.php
+++
@@ -1,3 +0,0 @@
-<?PHP exit;?>	1577771772	gaojunliang	VN***l	Ques #0	114.251.159.67
-<?PHP exit;?>	1577771781	gaojunliang	VN***l	Ques #0	114.251.159.67
-<?PHP exit;?>	1577771802	gaojunliang	VN***l	Ques #0	114.251.159.67
--- discuz/data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 451160b75d40c2cee218ce5e25347fc4
-
-$pluginsetting = array (
-);
-?>
